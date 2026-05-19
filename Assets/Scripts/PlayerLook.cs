f3154027c8563b422ff34bdef8fdaff74192b2cb
Guarni01 <andreag.lunetta@gmail.com>
2026-05-19 22:25:40 +0200

    Player_Controller

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSense = 200f;
+    public Transform cam;
+
+    private float xRotation = 0;
+    private Vector2 lookInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMouseLook();
+    }
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = lookInput.x * mouseSense * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSense * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+}
