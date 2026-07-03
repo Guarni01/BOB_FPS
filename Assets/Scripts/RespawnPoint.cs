0cf3dc47dfe2baaea0a4d1ed33f670ff0d5b5bff
Guarni01 <andreag.lunetta@gmail.com>
2026-07-03 15:08:04 +0200

    moriro giovane

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
+    public Vector3 spawnOffset;
+
     public Vector3 GetSpawnPosition()
     {
-        return transform.position;
+        return transform.position + spawnOffset;
     }
 
     public Quaternion GetSpawnRotation()
